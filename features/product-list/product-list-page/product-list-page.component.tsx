f0cd2f4dc6867ddef50a33a83a1a250163663b6c
Massimo <massipetra@hotmail.it>
2024-06-01 17:10:50 +0200

    git-flow initializated, blank components created and initializated

---
+++ features/product-list/product-list-page/product-list-page.component.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { ProductListView } from "../product-list-view/product-list-view.component";
+
+export const ProductListPage = () => {
+
+    return (
+        <View>
+            <ProductListView/>
+        </View>
+    )
+}
---
+++ features/product-list/product-list-view/product-list-view.component.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+export const ProductListView = () => {
+    return (
+        <View>
+            <Text>
+                product list work
+            </Text>
+        </View>
+    )
+}
